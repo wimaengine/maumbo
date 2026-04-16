d0900552941d7f7576965a5423946e4221f56374
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-04-16 10:02:00 +0300

    Add `Contact2D`

---
+++ src/core/contact.ts
@@ -0,0 +1,56 @@
+import { Affine2, Vector2 } from 'hisabati'
+
+export class Contact2D {
+  pointA = new Vector2()
+  pointB = new Vector2()
+  normalA = new Vector2()
+  normalB = new Vector2()
+  depth = 0
+  tangentA?: Vector2
+  tangentB?: Vector2
+
+  constructor(
+    pointA: Vector2,
+    pointB: Vector2,
+    normalA: Vector2,
+    normalB: Vector2,
+    depth: number,
+    tangentA?: Vector2,
+    tangentB?: Vector2
+  ) {
+    this.pointA = pointA
+    this.pointB = pointB
+    this.normalA = normalA
+    this.normalB = normalB
+    this.depth = depth
+    this.tangentA = tangentA
+    this.tangentB = tangentB
+  }
+
+  clone(): Contact2D {
+    return new Contact2D(this.pointA, this.pointB, this.normalA, this.normalB, this.depth)
+  }
+
+  transform(transformA: Affine2, transformB: Affine2): this {
+    transformA.transform(this.pointA)
+    transformB.transform(this.pointB)
+    this.normalA = Affine2.transformWithoutTranslation(transformA, this.normalA)
+    this.normalB = Affine2.transformWithoutTranslation(transformB, this.normalB)
+
+    // TODO: Transform tangents
+    return this
+  }
+
+  flip(): this {
+    const { pointA, normalA, tangentA } = this
+
+    this.pointA = this.pointB
+    this.pointB = pointA
+    this.normalA = this.normalB
+    this.normalB = normalA
+    this.tangentA = this.tangentB
+    this.tangentB = tangentA
+
+    return this
+  }
+}
