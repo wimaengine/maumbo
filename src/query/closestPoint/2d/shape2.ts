3d4f83833652c678911ca36a6141f90d950a77d5
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-04-16 16:12:00 +0300

    Add `getShape2ClosestPoints` function

---
+++ src/query/closestPoint/2d/shape2.ts
@@ -0,0 +1,23 @@
+import { Affine2 } from 'hisabati'
+import { Circle } from '../../../shapes'
+import { Shape2 } from '../../../shapes'
+import { getCircleClosestPoint } from './circle.js'
+import type { ClosestPoint2D } from '../../../core'
+
+export function getShape2ClosestPoints(
+  shapeA: Shape2,
+  shapeB: Shape2,
+  transformA: Affine2,
+  transformB: Affine2
+): ClosestPoint2D[] | undefined {
+  const transform = Affine2.multiply(
+    Affine2.invert(transformA, new Affine2()),
+    transformB
+  )
+  
+  if(shapeA instanceof Circle && shapeB instanceof Circle) {
+    return [getCircleClosestPoint(shapeA, shapeB, transform)]
+  }
+
+  return undefined
+}
