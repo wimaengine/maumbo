066a7cdec8cfe1c445a21d5d9e2d7297210314ce
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-04-17 09:27:00 +0300

    Add capsule contact generators

---
+++ src/query/contacts/2d/capsule.ts
@@ -0,0 +1,205 @@
+import { Contact2D, sat2dCapsule } from '../../../core'
+import { Capsule, Circle, ConvexPolygon, Rectangle, Triangle } from '../../../shapes'
+import { Vector2, clamp, Affine2 } from 'hisabati'
+import {
+  closestPointsSegmentSegment
+} from '../../closestPoint'
+import { getNearVertex } from './utils.js'
+
+/**
+ * @param {Capsule} capsuleA
+ * @param {Capsule} capsuleB
+ * @param {Affine2} transform
+ * @param {Affine2} invTransform
+ */
+export function capsuleContacts(
+  capsuleA: Capsule,
+  capsuleB: Capsule,
+  transform: Affine2,
+  invTransform: Affine2
+): Contact2D[] | undefined {
+  const a0 = new Vector2(0, capsuleA.halfHeight)
+  const a1 = new Vector2(0, -capsuleA.halfHeight)
+  const b0 = transform.transform(new Vector2(0, capsuleB.halfHeight))
+  const b1 = transform.transform(new Vector2(0, -capsuleB.halfHeight))
+  const radiusSum = capsuleA.radius + capsuleB.radius
+  const { pA, pB } = closestPointsSegmentSegment(a0, a1, b0, b1)
+  const delta = Vector2.subtract(pB, pA)
+  const separation = delta.magnitude()
+  const closest = [
+    {
+      pointA: pA,
+      pointB: pB,
+      distance: separation
+    }
+  ]
+  const contacts = closest
+    .map((point) => {
+    const distance = radiusSum - point.distance
+
+    if (distance < 0) {
+      return undefined
+    }
+
+    const normalA = separation !== 0
+      ? Vector2.divideScalar(delta, separation)
+      : Vector2.Y.clone()
+    const normalB = Affine2.transformWithoutTranslation(invTransform, normalA).reverse()
+    const pointB = invTransform.transform(point.pointB)
+
+    return new Contact2D(
+      Vector2.multiplyScalar(normalA, capsuleA.radius).add(point.pointA),
+      Vector2.multiplyScalar(normalB, capsuleB.radius).add(pointB),
+      normalA,
+      normalB,
+      distance
+    )
+    })
+    .filter((e): e is Contact2D => e !== undefined)
+
+  if (!contacts.length) {
+    return undefined
+  }
+
+  return contacts
+}
+
+/**
+ * @param {Capsule} capsule
+ * @param {Circle} circle
+ * @param {Affine2} transform
+ * @param {Affine2} invTransform
+ */
+export function capsuleCircleContact(
+  capsule: Capsule,
+  circle: Circle,
+  transform: Affine2,
+  invTransform: Affine2
+): Contact2D | undefined {
+  const position = new Vector2(
+    transform.x,
+    transform.y
+  )
+  const lineStart = new Vector2(0, capsule.halfHeight)
+  const lineEnd = new Vector2(0, -capsule.halfHeight)
+
+  const radiusSum = circle.radius + capsule.radius
+  const l1 = Vector2.subtract(lineEnd, lineStart)
+  const l2 = Vector2.subtract(position, lineStart)
+  const lenSq = l1.magnitudeSquared()
+  const t = clamp(Vector2.dot(l1, l2) / lenSq, 0, 1)
+  const closest = Vector2.multiplyScalar(l1, t).add(lineStart)
+  const dist = Vector2.subtract(position, closest)
+  const distSq = dist.magnitudeSquared()
+
+  if (distSq > radiusSum * radiusSum) {
+    return undefined
+  }
+
+  const distance = Math.sqrt(distSq)
+  const penetration = radiusSum - distance
+
+  const normalA = distance !== 0 ? Vector2.divideScalar(dist, distance) : Vector2.Y.clone()
+  const normalB = Affine2.transformWithoutTranslation(invTransform, normalA).reverse()
+
+  return new Contact2D(
+    Vector2.multiplyScalar(normalA, capsule.radius).add(closest),
+    Vector2.multiplyScalar(normalB, circle.radius),
+    normalA,
+    normalB,
+    penetration
+  )
+}
+
+/**
+ * @param {Capsule} a   // body A
+ * @param {Rectangle} b // body B (OBB)
+ * @param {Affine2} transform     // B -> A
+ * @param {Affine2} invTransform  // A -> B
+ */
+export function capsuleRectangleContact(
+  a: Capsule,
+  b: Rectangle,
+  transform: Affine2,
+  invTransform: Affine2
+): ReturnType<typeof sat2dCapsule> {
+  const points = b.getPoints().map((e: Vector2) => transform.transform(e.clone()))
+  const nearestIndex = getNearVertex(Vector2.Zero, points)
+  const axis = Vector2.copy(points[nearestIndex])
+  const length = axis.magnitudeSquared()
+
+  if (length === 0) {
+    axis.set(transform.x, transform.y)
+  }
+
+  axis.normalize()
+  const axes = [
+    axis,
+    Vector2.normal(Vector2.subtract(points[0], points[1])).normalize(),
+    Vector2.normal(Vector2.subtract(points[1], points[2])).normalize()
+  ]
+
+  return sat2dCapsule(a, points, axes, transform, invTransform)
+}
+
+/**
+ * @param {Capsule} a    // body A
+ * @param {Triangle} b  // body B
+ * @param {Affine2} transform     // B -> A
+ * @param {Affine2} invTransform  // A -> B
+ */
+export function capsuleTriangleContact(
+  a: Capsule,
+  b: Triangle,
+  transform: Affine2,
+  invTransform: Affine2
+): ReturnType<typeof sat2dCapsule> {
+  const points = b.getPoints().map((e: Vector2) => transform.transform(e.clone()))
+  const nearestIndex = getNearVertex(Vector2.Zero, points)
+  const axis = Vector2.copy(points[nearestIndex])
+  const length = axis.magnitudeSquared()
+
+  if (length === 0) {
+    axis.set(transform.x, transform.y)
+  }
+
+  axis.normalize()
+  const axes = [
+    axis,
+    Vector2.normal(Vector2.subtract(points[0], points[1])).normalize(),
+    Vector2.normal(Vector2.subtract(points[1], points[2])).normalize(),
+    Vector2.normal(Vector2.subtract(points[2], points[0])).normalize()
+  ]
+
+  return sat2dCapsule(a, points, axes, transform, invTransform)
+}
+
+/**
+ * @param {Capsule} a          // body A
+ * @param {ConvexPolygon} b   // body B
+ * @param {Affine2} transform     // B -> A
+ * @param {Affine2} invTransform  // A -> B
+ */
+export function capsuleConvexPolygonContact(
+  a: Capsule,
+  b: ConvexPolygon,
+  transform: Affine2,
+  invTransform: Affine2
+): ReturnType<typeof sat2dCapsule> {
+  const points = b.points.map((e: Vector2) => transform.transform(e.clone()))
+  const nearestIndex = getNearVertex(Vector2.Zero, points)
+  const axis = Vector2.copy(points[nearestIndex])
+  const length = axis.magnitudeSquared()
+
+  if (length === 0) {
+    axis.set(transform.x, transform.y)
+  }
+
+  axis.normalize()
+  const axes = [
+    axis,
+    ...b.normals.map((e: Vector2) => Affine2.transformWithoutTranslation(transform, e))
+  ]
+
+  return sat2dCapsule(a, points, axes, transform, invTransform)
+}
