56ecffdcd1c8f39105e931bb646db5d7bcdcfb1f
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-04-22 17:31:00 +0300

    Add test for triangle

---
+++ tests/shapes/triangle.test.js
@@ -0,0 +1,23 @@
+import test from "node:test"
+import { deepStrictEqual, strictEqual } from "node:assert"
+import { Vector2 } from "hisabati"
+import { Shape2, Triangle } from "../../dist/index.module.js"
+
+test("Triangle stores dimensions and default baseRatio", () => {
+  const triangle = new Triangle(2, 3)
+
+  strictEqual(triangle.halfBase, 2)
+  strictEqual(triangle.halfHeight, 3)
+  strictEqual(triangle.baseRatio, 0)
+  strictEqual(triangle instanceof Shape2, true)
+})
+
+test("Triangle.getPoints uses baseRatio for the apex", () => {
+  const triangle = new Triangle(2, 3, 0.5)
+
+  deepStrictEqual(triangle.getPoints(), [
+    new Vector2(-2, -3),
+    new Vector2(2, -3),
+    new Vector2(1, 3),
+  ])
+})
