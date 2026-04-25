2af8ff443ec90fdf629f3b32fe7898f1c16990bb
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-04-25 18:26:21 +0300

    Add cuboid shape

---
+++ src/shapes/3d/cuboid.ts
@@ -0,0 +1,11 @@
+export class Cuboid {
+  halfWidth
+  halfHeight
+  halfLength
+
+  constructor(halfWidth: number, halfHeight: number, halfLength: number) {
+    this.halfWidth = halfWidth
+    this.halfHeight = halfHeight
+    this.halfLength = halfLength
+  }
+}
