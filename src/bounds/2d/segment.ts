4b6680efc9e2573acbf5e1fb885709e2c6013464
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-04-26 17:21:11 +0300

    Add `Segment2D`

---
+++ src/bounds/2d/segment.ts
@@ -0,0 +1,67 @@
+import { Vector2 } from 'hisabati'
+
+/**
+ * A 2d segment.
+ */
+export class Segment2D {
+
+  /**
+   * The start of the segment.
+   *
+   * @type {Vector2}
+   */
+  start
+
+  /**
+   * The end of the segment.
+   *
+   * @type {Vector2}
+   */
+  end
+
+  constructor(minX = 0, minY = 0, maxX = 0, maxY = 0) {
+    this.start = new Vector2(minX, minY)
+    this.end = new Vector2(maxX, maxY)
+  }
+
+  translate(x:number, y:number) {
+    return Segment2D.translate(this, x, y, this)
+  }
+
+  /**
+   * Deep copies a bounding box to a new one.
+   *
+   * @deprecated
+   * @returns {Segment2D}
+   */
+  clone() {
+    return Segment2D.copy(this)
+  }
+
+  /**
+   * Deep copies another bounding box.
+   */
+  copy(bounds:Segment2D) {
+    Segment2D.copy(bounds, this)
+  }
+
+  static copy(bound:Segment2D, out = new Segment2D()) {
+    out.end.x = bound.end.x
+    out.end.y = bound.end.y
+    out.start.x = bound.start.x
+    out.start.y = bound.start.y
+
+    return out
+  }
+
+  /**
+   */
+  static translate(bound:Segment2D, x:number, y:number, out = new Segment2D()) {
+    out.end.x = bound.end.x + x
+    out.end.y = bound.end.y + y
+    out.start.x = bound.start.x + x
+    out.start.y = bound.start.y + y
+
+    return out
+  }
+}
