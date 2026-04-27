3d5aba872b987d17057fc77f6dc410ecbc2bf127
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-04-27 19:09:39 +0300

    Add intersection queries for `ConvexPolygon`

---
+++ src/query/intersection/2d/polygon.ts
@@ -0,0 +1,40 @@
+import { Affine2 } from 'hisabati'
+import { Circle, ConvexPolygon, Rectangle, Triangle } from '../../../shapes'
+import { Intersection2D } from '../../../core'
+import { intersectShapes2d } from './utils.js'
+
+export function polygonIntersections(
+  boxA: ConvexPolygon,
+  boxB: ConvexPolygon,
+  transform: Affine2,
+  _invTransform: Affine2
+): Intersection2D[] | undefined {
+  return intersectShapes2d(boxA, boxB, transform)
+}
+
+export function circlePolygonIntersection(
+  circle: Circle,
+  polygon: ConvexPolygon,
+  transform: Affine2,
+  _invTransform: Affine2
+): Intersection2D[] | undefined {
+  return intersectShapes2d(circle, polygon, transform)
+}
+
+export function polygonRectangleIntersections(
+  boxA: ConvexPolygon,
+  boxB: Rectangle,
+  transform: Affine2,
+  _invTransform: Affine2
+): Intersection2D[] | undefined {
+  return intersectShapes2d(boxA, boxB, transform)
+}
+
+export function polygonTriangleIntersections(
+  boxA: ConvexPolygon,
+  boxB: Triangle,
+  transform: Affine2,
+  _invTransform: Affine2
+): Intersection2D[] | undefined {
+  return intersectShapes2d(boxA, boxB, transform)
+}
