3e1ece3a32401a09ff40752f01980843b0648722
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-04-27 22:22:12 +0300

    Implement `Boundable2D` on `Line2`

--- src/shapes/2d/line.ts
+++ src/shapes/2d/line.ts
@@ -1,10 +1,27 @@
+import { BoundingBox2D, BoundingCircle, type Boundable2D } from '../../bounds/index.js'
 import { Shape2 } from './shape2.js'
 
-export class Line2 extends Shape2 {
+export class Line2 extends Shape2 implements Boundable2D {
   halfLength = 0
 
   constructor(halfLength: number) {
     super()
     this.halfLength = halfLength
+  }
+
+  aabb2d(): BoundingBox2D {
+    return new BoundingBox2D(
+      -this.halfLength,
+      -this.halfLength,
+      this.halfLength,
+      this.halfLength
+    )
+  }
+  boundingCircle(): BoundingCircle {
+    return new BoundingCircle(
+      0,
+      0,
+      this.halfLength
+    )
   }
 }
