153d5bebd330a6a5da5deaf0b6ff0caf68dadeaf
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-04-29 16:26:26 +0300

    Add `Shape3` union

---
+++ src/shapes/3d/shape3.ts
@@ -0,0 +1,7 @@
+import type { Cone } from "./cone";
+import type { ConvexPolyhedron } from "./convexPolyhedron";
+import type { Cuboid } from "./cuboid";
+import type { Cylinder } from "./cylinder";
+import type { Sphere } from "./sphere";
+
+export type Shape3 = ConvexPolyhedron | Sphere | Cuboid | Cylinder | Cone
