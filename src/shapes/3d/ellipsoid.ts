c41b54399dba2342d914f05178604f1d38b6d75a
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-04-29 20:04:04 +0300

    Add `Ellipsoid` shape

---
+++ src/shapes/3d/ellipsoid.ts
@@ -0,0 +1,11 @@
+export class Ellipsoid {
+    radiusX
+    radiusY
+    radiusZ
+
+    constructor(radiusX = 0.5, radiusY = 0.5, radiusZ = 0.5) {
+        this.radiusX = radiusX
+        this.radiusY = radiusY
+        this.radiusZ = radiusZ
+    }
+}
--- src/shapes/3d/shape3.ts
+++ src/shapes/3d/shape3.ts
@@ -2,6 +2,7 @@ import type { Cone } from "./cone";
 import type { ConvexPolyhedron } from "./convexPolyhedron";
 import type { Cuboid } from "./cuboid";
 import type { Cylinder } from "./cylinder";
+import type { Ellipsoid } from "./ellipsoid";
 import type { Sphere } from "./sphere";
 
-export type Shape3 = ConvexPolyhedron | Sphere | Cuboid | Cylinder | Cone
+export type Shape3 = ConvexPolyhedron | Sphere | Cuboid | Cylinder | Cone | Ellipsoid
