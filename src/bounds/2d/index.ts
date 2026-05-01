d4a3fa03c529452fc76c4d63ffde1a1aac81f9c4
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-05-01 16:09:03 +0300

    Add `Bounds`

--- src/bounds/2d/index.ts
+++ src/bounds/2d/index.ts
@@ -3,3 +3,8 @@ export * from './box'
 export * from './circle'
 export * from './segment'
 export * from './triangle'
+
+import type { BoundingBox2D } from './box'
+import type { BoundingCircle } from './circle'
+
+export type Bounds = BoundingBox2D | BoundingCircle
