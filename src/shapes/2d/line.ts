0161d033f48fb425542d367e2406502ce0fdbf9a
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-05-01 18:42:10 +0300

    Implement `SupportMapped2d` onto `Line2`

--- src/shapes/2d/line.ts
+++ src/shapes/2d/line.ts
@@ -1,10 +1,43 @@
+import { Vector2 } from 'hisabati'
 import { BoundingBox2D, BoundingCircle, type Boundable2D } from '../../bounds/index.js'
+import type { Feature, SupportMapped2d } from '../../core/clipping.js'
 
-export class Line2 implements Boundable2D {
+export class Line2 implements Boundable2D, SupportMapped2d {
   halfLength = 0
 
   constructor(halfLength: number) {
     this.halfLength = halfLength
+  }
+
+  getSupportPoint2d(direction: Vector2): Vector2 {
+    return (direction.x >= 0)
+      ? new Vector2(this.halfLength, 0)
+      : new Vector2(-this.halfLength, 0)
+  }
+
+  getFeature2d(direction: Vector2): Feature {
+    if (Math.abs(direction.y) > Math.abs(direction.x)) {
+      const normal = direction.y >= 0
+        ? Vector2.Y.clone()
+        : Vector2.Y.clone().reverse()
+
+      return {
+        type: 'edge',
+        v1: new Vector2(this.halfLength, 0),
+        v2: new Vector2(-this.halfLength, 0),
+        normal
+      }
+    }
+
+    const normal = direction.magnitudeSquared() === 0
+      ? Vector2.X.clone()
+      : direction.clone().normalize()
+
+    return {
+      type: 'point',
+      point: this.getSupportPoint2d(direction),
+      normal
+    }
   }
 
   aabb2d(): BoundingBox2D {
