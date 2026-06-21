17696480223153bdabb13bd9713105004a71a453
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-06-21 18:41:20 +0300

    Add transformation methods on `TriangleBound3D`

--- src/bounds/3d/triangle.ts
+++ src/bounds/3d/triangle.ts
@@ -1,4 +1,4 @@
-import { Vector3 } from 'hisabati'
+import { Affine3, Quaternion, Vector3 } from 'hisabati'
 
 /**
  * A 2d segment.
@@ -14,8 +14,16 @@ export class TriangleBound3D {
     this.c = c
   }
 
-  translate(x:number, y:number, z = 0) {
-    return TriangleBound3D.translate(this, x, y, z, this)
+  translate(translation: Vector3) {
+    return TriangleBound3D.translate(this, translation, this)
+  }
+
+  rotate(center: Vector3, rotation: Quaternion) {
+    return TriangleBound3D.rotate(this, center, rotation, this)
+  }
+
+  transform(affine: Affine3) {
+    return TriangleBound3D.transform(this, affine, this)
   }
 
   /**
@@ -51,16 +59,43 @@ export class TriangleBound3D {
 
   /**
    */
-  static translate(bound:TriangleBound3D, x:number, y:number, z = 0, out = new TriangleBound3D()) {
-    out.c.x = bound.c.x + x
-    out.c.y = bound.c.y + y
-    out.c.z = bound.c.z + z
-    out.b.x = bound.b.x + x
-    out.b.y = bound.b.y + y
-    out.b.z = bound.b.z + z
-    out.a.x = bound.a.x + x
-    out.a.y = bound.a.y + y
-    out.a.z = bound.a.z + z
+  static translate(bound:TriangleBound3D, translation: Vector3, out = new TriangleBound3D()) {
+    out.c.x = bound.c.x + translation.x
+    out.c.y = bound.c.y + translation.y
+    out.c.z = bound.c.z + translation.z
+    out.b.x = bound.b.x + translation.x
+    out.b.y = bound.b.y + translation.y
+    out.b.z = bound.b.z + translation.z
+    out.a.x = bound.a.x + translation.x
+    out.a.y = bound.a.y + translation.y
+    out.a.z = bound.a.z + translation.z
+
+    return out
+  }
+
+  /**
+   */
+  static rotate(bound:TriangleBound3D, center: Vector3, rotation: Quaternion, out = new TriangleBound3D()) {
+    const c = Vector3.subtract(bound.c, center, new Vector3())
+    const b = Vector3.subtract(bound.b, center, new Vector3())
+    const a = Vector3.subtract(bound.a, center, new Vector3())
+
+    Quaternion.transformVector3(rotation, c)
+    Quaternion.transformVector3(rotation, b)
+    Quaternion.transformVector3(rotation, a)
+    Vector3.add(c, center, out.c)
+    Vector3.add(b, center, out.b)
+    Vector3.add(a, center, out.a)
+
+    return out
+  }
+
+  /**
+   */
+  static transform(bound:TriangleBound3D, affine: Affine3, out = new TriangleBound3D()) {
+    Affine3.transform(affine, bound.c, out.c)
+    Affine3.transform(affine, bound.b, out.b)
+    Affine3.transform(affine, bound.a, out.a)
 
     return out
   }
