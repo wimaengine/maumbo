221f7eae56d6849bbd0604ae7021b3da0af7cad0
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-06-21 18:35:57 +0300

    Add transformation methods on `BoundingBox2D`

--- src/bounds/2d/box.ts
+++ src/bounds/2d/box.ts
@@ -1,4 +1,5 @@
 import { Affine2, Vector2 } from 'hisabati'
+import type { Rotary } from 'hisabati'
 
 /**
  * A 2d axis aligned bounding box.
@@ -24,12 +25,16 @@ export class BoundingBox2D {
     this.min = new Vector2(minX, minY)
   }
 
-  translate(x:number, y:number) {
-    return BoundingBox2D.translate(this, x, y, this)
+  translate(translation: Vector2) {
+    return BoundingBox2D.translate(this, translation, this)
+  }
+
+  rotate(center: Vector2, rotary: Rotary) {
+    return BoundingBox2D.rotate(this, center, rotary, this)
   }
 
   transform(affine: Affine2){
-    BoundingBox2D.transform(this,affine, this)
+    return BoundingBox2D.transform(this,affine, this)
   }
 
   /**
@@ -60,11 +65,46 @@ export class BoundingBox2D {
 
   /**
    */
-  static translate(bound:BoundingBox2D, x:number, y:number, out = new BoundingBox2D()) {
-    out.min.x = bound.min.x + x
-    out.min.y = bound.min.y + y
-    out.max.x = bound.max.x + x
-    out.max.y = bound.max.y + y
+  static translate(bound:BoundingBox2D, translation: Vector2, out = new BoundingBox2D()) {
+    out.min.x = bound.min.x + translation.x
+    out.min.y = bound.min.y + translation.y
+    out.max.x = bound.max.x + translation.x
+    out.max.y = bound.max.y + translation.y
+
+    return out
+  }
+
+  /**
+   */
+  static rotate(bound:BoundingBox2D, center: Vector2, rotary: Rotary, out = new BoundingBox2D()) {
+    const min = Vector2.subtract(bound.min, center, new Vector2())
+    const maxXMinY = Vector2.subtract(new Vector2(bound.max.x, bound.min.y), center, new Vector2())
+    const max = Vector2.subtract(bound.max, center, new Vector2())
+    const minXMaxY = Vector2.subtract(new Vector2(bound.min.x, bound.max.y), center, new Vector2())
+
+    Vector2.rotateFast(min, rotary.cos, rotary.sin, min)
+    Vector2.rotateFast(maxXMinY, rotary.cos, rotary.sin, maxXMinY)
+    Vector2.rotateFast(max, rotary.cos, rotary.sin, max)
+    Vector2.rotateFast(minXMaxY, rotary.cos, rotary.sin, minXMaxY)
+    Vector2.add(min, center, min)
+    Vector2.add(maxXMinY, center, maxXMinY)
+    Vector2.add(max, center, max)
+    Vector2.add(minXMaxY, center, minXMaxY)
+
+    let outMinX = min.x
+    let outMinY = min.y
+    let outMaxX = min.x
+    let outMaxY = min.y
+
+    outMinX = Math.min(outMinX, maxXMinY.x, max.x, minXMaxY.x)
+    outMinY = Math.min(outMinY, maxXMinY.y, max.y, minXMaxY.y)
+    outMaxX = Math.max(outMaxX, maxXMinY.x, max.x, minXMaxY.x)
+    outMaxY = Math.max(outMaxY, maxXMinY.y, max.y, minXMaxY.y)
+
+    out.min.x = outMinX
+    out.min.y = outMinY
+    out.max.x = outMaxX
+    out.max.y = outMaxY
 
     return out
   }
@@ -103,7 +143,6 @@ export class BoundingBox2D {
     out.max.y = bound1.max.y > bound2.max.y ? bound1.max.y : bound2.max.y
     out.min.x = bound1.min.x < bound2.min.x ? bound1.min.x : bound2.min.x
     out.min.y = bound1.min.y < bound2.min.y ? bound1.min.y : bound2.min.y
-
     return out
   }
 }
