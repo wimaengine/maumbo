ccc4f64968cceb5969c2ad7a35db171ec7d8a1e7
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-06-21 18:38:03 +0300

    Add `BoundaryPrimitive2D` union

--- src/bounds/2d/index.ts
+++ src/bounds/2d/index.ts
@@ -7,5 +7,8 @@ export * from './arc'
 
 import type { BoundingBox2D } from './box'
 import type { BoundingCircle } from './circle'
+import type { Segment2D } from './segment'
+import type { ArcBound2D } from './arc'
 
 export type Bounds = BoundingBox2D | BoundingCircle
+export type BoundaryPrimitive2D = Segment2D | ArcBound2D
