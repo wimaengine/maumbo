3ca2bd2843da7f0c5c898eca81093212bc6c69f5
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-06-21 18:37:26 +0300

    Add transformation methods on `Segment2D`

--- src/bounds/2d/segment.ts
+++ src/bounds/2d/segment.ts
@@ -1,4 +1,5 @@
-import { Vector2 } from 'hisabati'
+import { Affine2, Vector2 } from 'hisabati'
+import type { Rotary } from 'hisabati'
 
 /**
  * A 2d segment.
@@ -24,8 +25,16 @@ export class Segment2D {
     this.end = end
   }
 
-  translate(x:number, y:number) {
-    return Segment2D.translate(this, x, y, this)
+  translate(translation: Vector2) {
+    return Segment2D.translate(this, translation, this)
+  }
+
+  rotate(center: Vector2, rotary: Rotary) {
+    return Segment2D.rotate(this, center, rotary, this)
+  }
+
+  transform(affine: Affine2) {
+    return Segment2D.transform(this, affine, this)
   }
 
   /**
@@ -56,11 +65,34 @@ export class Segment2D {
 
   /**
    */
-  static translate(bound:Segment2D, x:number, y:number, out = new Segment2D()) {
-    out.end.x = bound.end.x + x
-    out.end.y = bound.end.y + y
-    out.start.x = bound.start.x + x
-    out.start.y = bound.start.y + y
+  static translate(bound:Segment2D, translation: Vector2, out = new Segment2D()) {
+    out.end.x = bound.end.x + translation.x
+    out.end.y = bound.end.y + translation.y
+    out.start.x = bound.start.x + translation.x
+    out.start.y = bound.start.y + translation.y
+
+    return out
+  }
+
+  /**
+   */
+  static rotate(bound:Segment2D, center: Vector2, rotary: Rotary, out = new Segment2D()) {
+    const end = Vector2.subtract(bound.end, center, new Vector2())
+    const start = Vector2.subtract(bound.start, center, new Vector2())
+
+    Vector2.rotateFast(end, rotary.cos, rotary.sin, end)
+    Vector2.rotateFast(start, rotary.cos, rotary.sin, start)
+    Vector2.add(end, center, out.end)
+    Vector2.add(start, center, out.start)
+
+    return out
+  }
+
+  /**
+   */
+  static transform(bound:Segment2D, affine: Affine2, out = new Segment2D()) {
+    Affine2.transform(affine, bound.end, out.end)
+    Affine2.transform(affine, bound.start, out.start)
 
     return out
   }
